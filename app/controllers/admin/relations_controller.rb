3a89ee2fb1c6790084a77a71e77cf71eeda1b684
Spencer Steffen <spencer@citrusme.com>
2011-06-08 14:40:36 -0700

    resourced relations controller

--- app/controllers/admin/relations_controller.rb
+++ app/controllers/admin/relations_controller.rb
@@ -2,7 +2,15 @@ class Admin::RelationsController < Admin::ResourceController
 
   before_filter :load_data, :only => :create
 
-  # [todo] fix me!
+  def create
+    @relation.relatable = @product
+    @relation.related_to = Variant.find(params[:relation][:related_to_id]).product
+    if @relation.save
+      render :partial => "admin/products/related_products_table", :locals => {:product => @product}, :layout => false
+    else
+      render :text => @relation.errors.inspect
+    end
+  end
 
   private
   
